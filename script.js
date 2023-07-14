34ec6eff928efc1b99ec82238cbbaf7f0c056c82
Dacian <dacianurban13@gmail.com>
2023-07-15 01:08:29 +0300

    day3

--- script.js
+++ script.js
@@ -3,6 +3,24 @@ function drop() {
     if (dropdownMenu.style.display === "none") {
         dropdownMenu.style.display = "flex";
     } else {
-        dropdownMenu.style.display = "none"; // Ascunde meniul dropdown dacă este deja afișat
+        dropdownMenu.style.display = "none";
     }
+}
+function scrollToward(){
+    const position = document.getElementById('urlBox');
+    position.scrollIntoView({behavior: 'smooth', block:"center"})
+}
+
+async function generate(){
+    const data = document.getElementById("urlBox").value;
+    const URL = 'https://shrtco.de/';
+    const request = await fetch(URL, {
+        method: "POST",
+        "Content-Type": "application/json",
+        cors: "cors",
+        body: JSON.stringify(data),
+    });
+    console.log(request);
+    const response = await request.json();
+    console.log(response);
 }
