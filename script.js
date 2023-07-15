85b1f906faa9245d934ba0a18d5a272a034dbe78
Dacian <dacianurban13@gmail.com>
2023-07-15 10:21:16 +0300

    day3

--- script.js
+++ script.js
@@ -13,14 +13,39 @@ function scrollToward(){
 
 async function generate(){
     const data = document.getElementById("urlBox").value;
-    const URL = 'https://shrtco.de/';
+    const URL = 'https://api.shrtco.de/v2/shorten?url=' +data;
     const request = await fetch(URL, {
-        method: "POST",
-        "Content-Type": "application/json",
-        cors: "cors",
-        body: JSON.stringify(data),
+        method: "GET",
+        mode: "cors",
     });
-    console.log(request);
     const response = await request.json();
-    console.log(response);
+    createDiv(data, response.result.short_link);
+}
+
+function createDiv(initial, url){
+    const p1 = document.createElement("p");
+    const div = document.createElement("div");
+    const div1 = document.createElement("div");
+    const p = document.createElement("p");
+    const btn = document.createElement("button");
+    p.textContent = url;
+    p1.textContent = initial;
+    div.className = "newDivs";
+    div1.className = "linksContainer";
+    btn.className = "copyBtn";
+    btn.textContent = "Copy";
+    btn.addEventListener("click",async ()=>{
+        await navigator.clipboard.writeText(url);
+        btn.textContent = "Copied";
+        btn.style.backgroundColor = "hsl(257, 27%, 26%)";
+        const time = setTimeout(()=>{
+            btn.textContent = "Copy";
+            btn.style.backgroundColor = "hsl(180, 66%, 49%)";
+        }, 3000);
+    });
+    div1.appendChild(p1)
+    div1.appendChild(p);
+    div.appendChild(div1);
+    div.appendChild(btn);
+    document.getElementById("shortedLinks").appendChild(div);
 }
